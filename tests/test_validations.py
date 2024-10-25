67d7e581a60cb8b6407ecfe5ea9063efa5fa31fc
Alexander Dardouri <alexanderdardouri@gmail.com>
2024-10-25 21:41:26 +0200

    style: pylint suggestions

--- tests/test_validations.py
+++ tests/test_validations.py
@@ -1,8 +1,15 @@
+'''
+Test main.py input validation
+'''
+
 import pytest
 import click
 from deadline_wizard.main import validate_time, validate_task
 
 def test_validate_time():
+    '''
+    Test for Validate time format for -t argument.
+    '''
     assert validate_time(None, None, '12:30') == '12:30'
     assert validate_time(None, None, '12:30 PM') == '12:30 PM'
     assert validate_time(None, None, '1230') == '1230'
@@ -15,9 +22,12 @@ def test_validate_time():
         validate_time(None, None, 'invalid_time')  # Example invalid time format
 
 def test_validate_task():
+    '''
+    Test for task input validation
+    '''
     assert validate_task(None, None, 'My Task') == 'My Task'
 
     # Check overly long inputs
     with pytest.raises(click.BadParameter):
-        validate_task(None, None, 'A very long task exceeding the length limit for tasks you should use to trigger.' 
+        validate_task(None, None, 'A very long task exceeding the length limit. '
         'It is definitely longer than 100 characters lalalalalalalalalalalalala...')
